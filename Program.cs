089480fede0f9ed3e35f55be027605c0ab3ec492
Elizaveta1094 <treninskaya.elizaveta@yandex.ru>
2022-11-22 16:17:25 +0300

    task 10

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.Write("Введите трехзначное число: " );
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>999 || number<99)
+{
+    Console.WriteLine("Введите трехзначное число");
+}
+else
+{
+    int digital1 = number/10;
+    int num = digital1%10;
+    Console.WriteLine(num);
+}
